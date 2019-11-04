23b14c61a8cfb47a4c744ecd5e18830f98df9754
Joeri <joeri_ruizendaal@hotmail.com>
2019-11-04 16:37:24 +0100

    Implemented support for multi-hit moves And added superdash

--- projects/dbfzComboTool.js
+++ projects/dbfzComboTool.js
@@ -6,112 +6,135 @@ var moves = [
     // Ground normals
     {
         "id": "5L",
-        "damage": 400,
+        "damage": [400],
         "proration": 1,
         "scaleTable": scaleTableLight
     },
     {
         "id": "5LL",
-        "damage": 700,
+        "damage": [700],
         "proration": 1,
         "scaleTable": scaleTableMedium
     },
     {
         "id": "5LLL",
-        "damage": 1000,  // TODO: might be 850 in certain situations? (and is character dependant)
+        "damage": [1000],  // TODO: might be 850 in certain situations? (and is character dependant)
         "proration": 1,
         "scaleTable": scaleTableMedium
     },
     {
         "id": "5M",
-        "damage": 700,
+        "damage": [700],
         "proration": 1,
         "scaleTable": scaleTableMedium
     },
     {
         "id": "5H",
-        "damage": 850,
+        "damage": [850],
         "proration": 1,
         "scaleTable": scaleTableMedium
     },
     {
         "id": "5S",
-        "damage": 300,
+        "damage": [300],
         "proration": 1,
         "scaleTable": scaleTableLight
     },
     {
         "id": "2L",
-        "damage": 400,
+        "damage": [400],
         "proration": 1,
         "scaleTable": scaleTableLight
     },
     {
         "id": "2M",
-        "damage": 700,
+        "damage": [700],
         "proration": 1,
         "scaleTable": scaleTableMedium
     },
     {
         "id": "2H",
-        "damage": 850,
+        "damage": [850],
         "proration": 1,
         "scaleTable": scaleTableMedium
     },
     {
         "id": "6M",
-        "damage": 850,
+        "damage": [850],
         "proration": 1,
         "scaleTable": scaleTableOverhead
     },
+
     // Air Normals
     {
         "id": "j.5L",
-        "damage": 400,
+        "damage": [400],
         "proration": 1,
         "scaleTable": scaleTableLight
     },
     {
         "id": "j.5LL",
-        "damage": 700,
+        "damage": [700],
         "proration": 1,
         "scaleTable": scaleTableLight
     },
     {
         "id": "j.5LLL",
-        "damage": 850,
+        "damage": [850],
         "proration": 1,
         "scaleTable": scaleTableLight
     },
     {
         "id": "j.5LLL (smash)",
-        "damage": 1000, // Should be 1000, need to check in-game
+        "damage": [1000], // Should be 1000, need to check in-game
         "proration": 1,
         "scaleTable": scaleTableLight // impossible to trigger in-game, so this is actually unknown
     },
     {
         "id": "j.5M",
-        "damage": 700,
+        "damage": [700],
         "proration": 1,
         "scaleTable": scaleTableLight
     },
     {
         "id": "j.5H",
-        "damage": 850,
+        "damage": [850],
         "proration": 1,
         "scaleTable": scaleTableLight
     },
     {
         "id": "j.2H",
-        "damage": 850,
+        "damage": [850],
         "proration": 1,
         "scaleTable": scaleTableLight
     },
     {
         "id": "j.5H (smash)",
-        "damage": 1000,
+        "damage": [1000],
         "proration": 1,
         "scaleTable": scaleTableLight // impossible to trigger in-game, so this is actually unknown
+    },
+
+    // Specials
+    {
+        "id": "Vegito 236L",
+        "damage": [130, 130, 130, 600],
+        "proration": 2,
+        "scaleTable": scaleTableMedium
+    },
+    {
+        "id": "Vegito 236L (2nd hit only)",
+        "damage": [600],
+        "proration": 1,
+        "scaleTable": scaleTableMedium
+    },
+
+    // Universal
+    {
+        "id": "superdash",
+        "damage": [300],
+        "proration": 2,
+        "scaleTable": scaleTableLight
     },
 ];
 
@@ -123,7 +146,6 @@ var uniqueTableRowId = 0;
 
 function addMove(moveId) {
     comboMoveIds.push(moveId);
-
     redrawTable();
 }
 
@@ -143,24 +165,28 @@ function redrawTable() {
         let scaling = 10000; // gigantic init value, meant to be overwritten
 
         // Pick the current scaling or the last one in the array
-        if (proration < scaleTable.length) {
-            scaling = scaleTable[proration];
-        }
-        else {
-            scaling = scaleTable[scaleTable.length - 1];
-        }
+        scaling = calculateScaling(scaleTable, proration);
 
-        let scaledDamage = move.damage * scaling;
-        scaledDamage = Math.trunc(scaledDamage);    // Turn the floating point into an int, kinda
+        // Loop through every damage value in case of multi-hit moves
+        move.damage.forEach(function(currentHitDamage, index) {
+            let currentScaling = scaling;
 
-        comboCurrentDamage += scaledDamage;
+            if (index > 0) {
+                currentScaling = calculateScaling(scaleTable, proration + 1);
+            }
 
-        let scalingPercentage = Math.trunc(scaling * 100) + "%"
+            let scaledDamage = currentHitDamage * currentScaling;
+            scaledDamage = Math.trunc(scaledDamage);    // Turn the floating point into an int, kinda    TODO: Check if this rounding matches the game's rounding
 
-        // Create and add new row
-        let generatedDeleteButtonContent = "<input type='button' onclick='removeRowFromComboTable(this.parentNode.parentNode.children[0].innerHTML - 1);' value='X'>"
-        let newRowContent = "<tr> <td style='font-weight: bold;'>" + (moveNumberInCombo + 1) + "</td> <td>" + (proration + 1) + "</td> <td>" + move.id + "</td> <td>" + scaledDamage + "</td> <td>" + scalingPercentage + "</td> <td>" + comboCurrentDamage + "</td> <td>" + generatedDeleteButtonContent + " </tr>"
-        newTableDataContent += newRowContent;
+            comboCurrentDamage += scaledDamage;
+
+            let scalingPercentage = Math.trunc(currentScaling * 100) + "%"
+
+            // Create and add new row
+            let generatedDeleteButtonContent = "<input type='button' onclick='removeRowFromComboTable(this.parentNode.parentNode.children[0].innerHTML - 1);' value='X'>"
+            let newRowContent = "<tr> <td style='font-weight: bold;'>" + (moveNumberInCombo + 1) + "</td> <td>" + (proration + 1) + "</td> <td>" + move.id + "</td> <td>" + scaledDamage + "</td> <td>" + scalingPercentage + "</td> <td>" + comboCurrentDamage + "</td> <td>" + generatedDeleteButtonContent + " </tr>"
+            newTableDataContent += newRowContent;            
+        });
 
         proration += move.proration;
         ++moveNumberInCombo;
@@ -184,4 +210,17 @@ function removeRowFromComboTable(rowNumber) {
         comboMoveIds.splice(rowNumber, 1);
         redrawTable();
     }
+}
+
+function calculateScaling(scaleTable, proration) {
+    let scaling = null;
+
+    if (proration < scaleTable.length) {
+        scaling = scaleTable[proration];
+    }
+    else {
+        scaling = scaleTable[scaleTable.length - 1];
+    }
+
+    return scaling;
 }
